4b11d3fa2ccfe2efdbf6995c29375923021d8e3c
mansi2011 <srivastavamansi91@gmail.com>
2020-12-20 17:34:16 +0530

    logical part completed

--- App.js
+++ App.js
@@ -12,12 +12,39 @@ var outputDiv = document.querySelector("#output");
 
 //querySelector is the way to tell the browser to select this element which has id btn-translate
 //query selector works on the principle of css selector . it selects and returns the first element which matches the query.
-btnTranslate.addEventListener("click", () => {
+
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text){
+    return url + "?" +"text="+ text
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("something happened! try again after some time!")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+
+    fetch(constructURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedOutput = json.contents.translated;
+        outputDiv.innerText = translatedOutput;
+        // console.log(json.contents.translated)
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
     // console.log("clicked");
     // console.log("input Received:" , txtInput.value);
-    outputDiv.innerText = "hfgjdsufusf " + txtInput.value;
+    // outputDiv.innerText = "hfgjdsufusf " + txtInput.value;
 
-})
+
 
 
 
